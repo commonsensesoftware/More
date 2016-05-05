76631e3033139b16562026318ed23e0b23021125
Chris Martinez <sydefekt@hotmail.com>
2016-05-05 14:39:33 -0700

    Updated version numbers

--- src/Hosting/AssemblyInfo.cs
+++ src/Hosting/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Reflection;
 [assembly: AssemblyTitle( "More.UI.Hosting" )]
 [assembly: AssemblyProduct( "More.UI.Hosting" )]
 [assembly: AssemblyDescription( "The user interface hosting library for the \"More\" framework." )]
-[assembly: AssemblyVersion( "1.3.0.0" )]
-[assembly: AssemblyInformationalVersion( "1.3.0" )]
+[assembly: AssemblyVersion( "1.4.0.0" )]
+[assembly: AssemblyInformationalVersion( "1.4.0" )]
